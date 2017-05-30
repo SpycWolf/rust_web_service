51015fd773be80b7c6b0afbc2421b1d2f967f51e
kota.kaneko <kota.kaneko@livesense.co.jp>
2017-05-31 00:00:26 +0900

    add router base logic.

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,46 @@
 extern crate iron;
 
+use std::collections::HashMap;
+
 use iron::prelude::*;
+use iron::{Handler};
 use iron::status;
 
+struct Router {
+    routes: HashMap<String, Box<Handler>>
+}
+
+impl Router {
+    fn new() -> Self{
+        Router { routes: HashMap::new() }
+    }
+    fn add_route<H>(&mut self, path: String, handler: H) where H: Handler {
+        self.routes.insert(path, Box::new(handler));
+    }
+}
+
+impl Handler for Router {
+    fn handle(&self, req: &mut Request) -> IronResult<Response> {
+        match self.routes.get(&req.url.path().join("/")) {
+            Some(handler) => handler.handle(req),
+            None => Ok(Response::with(status::NotFound))
+        }
+    }
+}
+
 fn main() {
-    Iron::new(|_: &mut Request| {
-        Ok(Response::with((status::Ok, "Status OK.")))
-    }).http("localhost:3000").unwrap();
+    let mut router = Router::new();
+
+    router.add_route("hello".to_string(), |_: &mut Request| {
+        Ok(Response::with((status::Ok, "Hello World")))
+    });
+
+    router.add_route("error".to_string(), |_: &mut Request| {
+        Ok(Response::with(status::BadRequest))
+    });
+
+    let host = "localhost:3000";
+    
+    println!("binding on {}", host);
+    Iron::new(router).http(host).unwrap();
 }
